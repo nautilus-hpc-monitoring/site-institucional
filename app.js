ce1ae3e57f6283eb9832ab06c505bc1d638bc78b
MarcellyPereira007 <marcelly.silva@sptech.school>
2026-09-08 01:10:25 -0300

    feat: Adicionando session storage e aplicando  entre as páginas protegidas

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ var usuarioRouter = require("./src/routes/usuarios");
 var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
 var nodesRouter = require("./src/routes/nodes");
 var empresaRouter = require("./src/routes/empresa");
 
@@ -36,7 +35,6 @@ app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
 app.use("/nodes", nodesRouter);
 app.use("/empresa", empresaRouter);
 
