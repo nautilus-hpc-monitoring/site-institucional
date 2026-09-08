2f8d376a8023148870c4a3ab07c53224256f03c0
MatheusDTavares <matheus.tavares@sptech.school>
2026-09-08 00:13:13 -0300

    Feat: Criando as pastas do backend para lista de usuario

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
 var cduserRouter = require("./src/routes/cduser");
+var qtdFuncionario = require("./src/routes/qtdFuncionario");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,6 +37,7 @@ app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
 app.use("/cduser", cduserRouter);
+app.use("/qtdFuncionario", qtdFuncionario);
 
 app.listen(PORTA_APP, function () {
     console.log(`
